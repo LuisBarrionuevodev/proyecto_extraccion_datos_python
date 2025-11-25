4bba29f09c993b57902d75d8c060e8cb9477c83b
LuisBarrionuevodev <luisbarrionuevo.dev@gmail.com>
2025-11-25 11:50:14 -0300

    primer commit

---
+++ bank_proyect.py
@@ -0,0 +1,9 @@
+URL = "https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
+
+table_attributes = ["Country,GDP(US$MM)"]
+
+db_name = "World_Economies.db"
+
+table_name = "Countries_by_GDP"
+
+csv_path = "./Countries_by_GDP.csv"
